81b2e607cedff5897ee988f90b6e704243e9ca30
Luigi Debusk <31847715+gwl080501109@users.noreply.github.com>
2024-08-27 11:10:04 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from utils import get_chat_response
+from langchain.memory import ConversationBufferMemory
+
+st.title("我的chatgpt")
+
+
+with st.sidebar:
+    mobile = st.text_input("请输入你的手机号")
+    st.info("*******************")
+if "memory" not in st.session_state:
+    st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
+    st.session_state["messages"] = [{"role":"ai",
+                                     "content":"你好，我是你的小助手，有什么可以帮你的吗"}]
+for message in st.session_state["messages"]:
+    st.chat_message(message["role"]).write(message["content"])
+
+prompt = st.chat_input()
+if prompt:
+    if not mobile:
+        st.info("请输入手机号")
+        st.stop()
+    st.session_state["messages"].append({"role":"human", "content":prompt})
+    st.chat_message("human").write(prompt)
+
+    with st.spinner("生成中,请稍等"):
+        response = get_chat_response(prompt, st.session_state["memory"])
+
+    msg = {"role":"ai",
+           "content": response}
+    st.session_state["messages"].append(msg)
+    st.chat_message("ai").write(response)
--- utils.py
+++ utils.py
@@ -1,50 +1,31 @@
 from langchain.prompts import ChatPromptTemplate
-from langchain_wenxin import ChatWenxin
+from langchain_community.llms.tongyi import Tongyi
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
 
 # 设置API密钥
 WENXIN_APP_Key = "oackgbiAt0I2PXRUzpaJP9E5"
 WENXIN_APP_SECRET = "HTIF206MaMZFIXKTlm6JbhtDfmSCosjD"
-
-
-def generate_script(subject, video_length,
-                    creativity):
-    title_template = ChatPromptTemplate.from_messages(
-        [
-            ("human", "请为'{subject}'这个主题的视频想一个吸引人的标题")
-        ]
-    )
-    script_template = ChatPromptTemplate.from_messages(
-        [
-            ("human",
-             """你是一位短视频频道的博主。根据以下标题和相关信息，为短视频频道写一个视频脚本。
-             视频标题：{title}，视频时长：{duration}分钟，生成的脚本的长度尽量遵循视频时长的要求。
-             要求开头抓住限球，中间提供干货内容，结尾有惊喜，脚本格式也请按照【开头、中间，结尾】分隔。
-             整体内容的表达方式要尽量轻松有趣，吸引年轻人。
-             脚本内容可以结合以下维基百科搜索出的信息，但仅作为参考，只结合相关的即可，对不相关的进行忽略：
-             ```{wikipedia_search}```""")
-        ]
+DASHSCOPE_API_KEY = "sk-7cb7535b25a54d5a8f3af0066af95fd3"
+model = Tongyi(
+        model_name="qwen-turbo",
+        max_tokens=1024,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        api_key=DASHSCOPE_API_KEY
     )
 
-    # 初始化文心模型
-    model = ChatWenxin(
-        temperature=creativity,
-        model="ernie-bot-turbo",
-        baidu_api_key=WENXIN_APP_Key,
-        baidu_secret_key=WENXIN_APP_SECRET,
-        verbose=True,
-    )
-    #title_template 这个得到的结果 传给模型传给模型
-    title_chain = title_template | model
-    script_chain = script_template | model
+def get_chat_response(prompt, memory):
+    chain = ConversationChain(llm=model, memory=memory)
 
-    title = title_chain.invoke({"subject": subject}).content
+    response = chain.invoke({"input":prompt})
+    print(response)
+    return response["response"]
 
-    search = WikipediaAPIWrapper(lang="zh")
-    search_result = search.run(subject)
-
-    script = script_chain.invoke({"title": title, "duration": video_length, "wikipedia_search": search_result}).content
-    return search_result, title, script
+# memory = ConversationBufferMemory(return_messages=True)
+# print(get_chat_response("比尔盖茨是谁", memory))
+# print(get_chat_response("我上一个问题问的是啥", memory))
 
 
-# print(generate_script("sora模型", 1, 0.7))
